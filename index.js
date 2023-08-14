b245f8c54c87c4c512ff6bdd029087633ef02949
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-14 15:33:44 -0400

    add tsconfig file and create first function addNums with string return type

---
+++ index.js
@@ -0,0 +1,6 @@
+var strawberry = 'fruit';
+var id = '29450';
+function addNums(num1, num2) {
+    return "".concat(num1 + num2);
+}
+console.log(addNums(1, 2))
---
+++ index.ts
@@ -0,0 +1,6 @@
+let strawberry = 'fruit';
+let id = '29450'
+
+function addNums(num1:number, num2: number): string {
+    return `${num1 + num2}`
+}
